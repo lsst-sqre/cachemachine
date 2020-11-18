f0e92a4319f78c9534f14f83a4e5aebeead5144e
Christine Banek <cbanek@gmail.com>
2020-11-18 12:17:12 -0700

    [DM-27441] Get available endpoint working for RubinRepoMan
    This generates the names of the various tags and things, and
    gets it inline with the changes that were made to the simplerepoman.

--- src/cachemachine/rubinrepoman.py
+++ src/cachemachine/rubinrepoman.py
@@ -31,39 +31,43 @@ class RubinRepoMan:
         releases = []
 
         for t in tags:
+            if self.registry_url == "hub.docker.com":
+                image_url = f"{self.repo}:{t}"
+            else:
+                image_url = f"{self.registry_url}/{self.repo}:{t}"
+
+            image_meta = {"tag": t, "image_url": image_url}
+
+            tag_parts = t.split("_")
+
             if t.startswith("d_"):
-                dailies.append(t)
+                # Ex: d_2020_11_02
+                image_meta["name"] = f"Daily {tag_parts[2]}/{tag_parts[3]}"
+                dailies.append(image_meta)
             elif t.startswith("w_"):
-                weeklies.append(t)
-            elif t.startswith("r_"):
-                releases.append(t)
-
-        chosen_tags = []
-
-        if "recommended" in tags:
-            chosen_tags.append("recommended")
-
-        dailies.sort(reverse=True)
-        weeklies.sort(reverse=True)
-        releases.sort(reverse=True)
-
-        chosen_tags.extend(self._prune(releases, self.num_releases))
-        chosen_tags.extend(self._prune(weeklies, self.num_weeklies))
-        chosen_tags.extend(self._prune(dailies, self.num_dailies))
+                # Ex: w_2020_41
+                image_meta["name"] = f"Weekly {tag_parts[2]}"
+                weeklies.append(image_meta)
+            elif t.startswith("r") and t != "recommended":
+                # Ex: r20_0_0, r20_0_0_rc1
+                image_meta["name"] = "Release " + ".".join(tag_parts)
+                releases.append(image_meta)
 
         images = []
-        for t in chosen_tags:
-            if self.registry_url == "hub.docker.com":
-                images.append(f"{self.repo}:{t}")
-            else:
-                images.append(f"{self.registry_url}/{self.repo}:{t}")
-
+        images.extend(self._prune(releases, self.num_releases))
+        images.extend(self._prune(weeklies, self.num_weeklies))
+        images.extend(self._prune(dailies, self.num_dailies))
         logger.info(f"Returning {images}")
-        return set(images)
+        return images
 
-    def _prune(self, tags, desired):
-        if len(tags) < desired:
-            logger.warning(f"{tags} too short to choose {desired}")
-            return tags
+    def _prune(self, image_metas, num_desired):
+        def take_tag(element):
+            return element["tag"]
+
+        image_metas = sorted(image_metas, key=take_tag, reverse=True)
+
+        if len(image_metas) < num_desired:
+            logger.warning(f"{image_metas} too short to choose {num_desired}")
+            return image_metas
         else:
-            return tags[0:desired]
+            return image_metas[0:num_desired]

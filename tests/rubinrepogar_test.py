4d95a0a075dbcbfe7f9fe9440ec3a035c312f316
dspeck1 <dspeck@burwood.com>
2022-03-31 09:52:52 -0500

    removed unused code

--- tests/rubinrepogar_test.py
+++ tests/rubinrepogar_test.py
@@ -11,54 +11,11 @@ from typing import List
 import pytest
 
 from cachemachine.rubinrepogar import RubinRepoGar
-from cachemachine.types import CachedDockerImage, DockerImage
-
-from .conftest import mock_registry
+from cachemachine.types import CachedDockerImage
 
 repo = (
     "us-central1-docker.pkg.dev/rubin-shared-services-71ec/sciplat/sciplat-lab"
 )
-
-expected_names = {
-    "prepuller_pulled_recommended": "prepuller_pulled_recommended",
-    "recommended": "Recommended (Release r21.0.0)",
-    "r23_0_1_rc4": "Release Candidate r23.0.1-rc4",
-    "r23_0_0": "Release r23.0.0",
-    "w_2022_10": "Weekly 2022_10",
-    "d_2022_03_04": "Daily 2022_03_04"
-    # "w_2021_21_c0021.003": "Weekly 2021_21 (SAL Cycle 0021, Build 003)",
-    # "w_2021_22_c0021.003_20210514": "Weekly 2021_21_20210514 (SAL Cycle 0021, Build 003)",  # noqa: E501
-    # "w_2021_22_20210514": "Weekly 2021_21_20210514",
-    # "w_2021_03": "Weekly 2021_03",
-    # "d_2021_01_13": "Daily 2021_01_13",
-    # "d_2021_01_12": "Daily 2021_01_12",
-}
-
-expected_all: List[DockerImage] = []
-for t in sorted(list(mock_registry.keys()), reverse=True):
-    expected_all.append(
-        # all_tags uses tag as name in dropdown
-        DockerImage(
-            image_url=f"{repo}:{t}", image_hash=mock_registry[t], name=t
-        )
-    )
-
-expected_desired_tags = [
-    "recommended",
-    "r23_0_0",
-    "w_2022_10",
-    "d_2022_03_04",
-]
-
-expected_desired: List[DockerImage] = []
-for t in expected_desired_tags:
-    expected_desired.append(
-        DockerImage(
-            image_url=f"{repo}:{t}",
-            image_hash=mock_registry[t],
-            name=expected_names[t],
-        )
-    )
 
 
 @pytest.mark.asyncio
